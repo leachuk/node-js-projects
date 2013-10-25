4785c20d4f94d059d5585eacecce0d01712b30fb
leachuk <stew.leach@gmail.com>
2013-10-25 16:54:44 +1100

    enabling backend checks for user signin and session storage.

--- phonegap-couchdb-project1/server.js
+++ phonegap-couchdb-project1/server.js
@@ -1,8 +1,10 @@
 var express = require("express");
 var app = express();
+var redis = require("redis").createClient();
 var requestHandlers = require("./requestHandlers");
 var nodejsUrl = "http://localhost:5984";
 
+ 
 //CORS Middleware
 var allowCrossDomain = function(request,response,next){
 	//console.log("processing access control");
@@ -10,11 +12,12 @@ var allowCrossDomain = function(request,response,next){
 	response.header("Access-Control-Allow-Headers", "X-Requested-With");
 	next();
 }
-app.configure(function(){
-	app.use(allowCrossDomain);
+app.configure(function(){	
 	app.use(express.cookieParser());
-	app.use(express.session({ secret: 'blabla' }));
+	app.use(express.session({secret: 'blabla'}));
 	app.use(express.bodyParser());
+	app.use(allowCrossDomain);
+
 });
 
 //API Routes
@@ -33,22 +36,18 @@ app.post("/signup", requestHandlers.signupUser);
 
 
 app.post('/api/user/signin', function (req, res) {
-    console.log("Session:" + req.session);
+    console.log("Session:");
     console.log("Auth deets:"+ req.body.name + "," + req.body.password);
-    console.log("Cookie deets:"+ req.cookies['AuthSession']);
     
-    /*
-    var auth = req.cookies['AuthSession'],
-      	nano;
-
-    if (!auth) { res.send(401); return; }
-	nano = require('nano')({ url : nodejsUrl, cookie: 'AuthSession=' + auth }),
-	username = req.body.name,
-	userpass = req.body.password;
-    */
     var nano = require('nano')({ url : nodejsUrl});
     var	username = req.body.name;
     var	userpass = req.body.password;
+//var username = "test4";
+//var userpass = "test";
+ //req.session.username = username;
+ redis.set("username", username);
+ redis.set("sid", req.session.id);
+
     nano.request({
             method: "POST",
             db: "_session",
@@ -56,13 +55,35 @@ app.post('/api/user/signin', function (req, res) {
             content_type: "application/x-www-form-urlencoded; charset=utf-8"
         },
         function (err, body, headers) {
-          console.log(headers);
+          //console.log(headers['set-cookie']);
             if (err) { res.send(err.reason); return; }
-
-            res.send('Logged in!');
+           
+            //res.send('Logged in!');
+            //res.send(headers['set-cookie']);
+            res.send(body);
         });
 });
 
+app.get('/api/user/verify', function (req, res) {
+	redis.get("username", function(err, reply){
+		if (reply){
+			var username = reply.toString();
+			if (username){
+				console.log(reply.toString());
+				res.send(reply.toString());
+			}
+		}
+	});
+	redis.get("sid", function(err, reply){
+		if (reply){
+			var sid = reply.toString();
+			if (sid){
+				console.log(reply.toString());
+				res.send(reply.toString());
+			}
+		}
+	});
+});
 
 app.listen(8888);
 console.log("Server has started.");
